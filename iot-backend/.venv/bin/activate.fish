8eb88d618ceb422c85a1522dba70dda4aa3cfba8
Leonardo Mrtz <carlosmroder@gmail.com>
2025-12-03 14:29:46 -0600

    nada imp

--- iot-backend/.venv/bin/activate.fish
+++ iot-backend/.venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/duffed/Documents/github/IoT/proyecto-iot/iot-backend/.venv"
+set -gx VIRTUAL_ENV "/Users/duffed/Documents/github/IoT/iot-backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
